f09b64e8403caaa5ad1233fd1b4bde4b72e36000
RobeJafet <robejafet@gmail.com>
2025-03-12 22:22:36 -0600

    fix conditional fixes

--- app/(frontend)/[lang]/[slug]/page.tsx
+++ app/(frontend)/[lang]/[slug]/page.tsx
@@ -1,10 +1,16 @@
 import Sections from "@/components/components/Sections";
-import { fetchSanityPageBySlug, } from "@/sanity/sevices/fetchPage";
+import { fetchSanityPageBySlug } from "@/sanity/sevices/fetchPage";
+import { generatePageMetadata } from "@/sanity/sevices/generateMetadata";
 
 
+type ParamsMetadata = Promise<{ lang: Locale; slug: string }>
 
-export const dynamicParams = false;
-
+export async function generateMetadata(props: {params: ParamsMetadata}) {
+    const paramsMetadata = await props.params;
+    const { lang, slug } =  paramsMetadata; 
+    const page = await fetchSanityPageBySlug(slug);
+    return generatePageMetadata({ page, slug, locale: lang });
+}
 
 type ParamsPage = Promise<{ slug: string }>
 
